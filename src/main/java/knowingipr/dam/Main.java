91893084c56f10eefca6ea61334dd287517ccd11
Silencer123456 <mydi148gacy>
2019-02-11 11:26:03 +0100

    Added basic UI

--- src/main/java/knowingipr/dam/Main.java
+++ src/main/java/knowingipr/dam/Main.java
@@ -2,18 +2,39 @@ package knowingipr.dam;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import knowingipr.dam.controller.DetailController;
+import knowingipr.dam.controller.ListController;
+import knowingipr.dam.model.DataModel;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        BorderPane root = new BorderPane();
+
+        FXMLLoader listLoader = new FXMLLoader(getClass().getResource("/list.fxml"));
+        root.setCenter(listLoader.load());
+        ListController listController = listLoader.getController();
+
+        FXMLLoader detailLoader = new FXMLLoader(getClass().getResource("/detail.fxml"));
+        root.setRight(detailLoader.load());
+        DetailController detailController = detailLoader.getController();
+
+        DataModel model = new DataModel();
+        listController.initModel(model);
+        detailController.initModel(model);
+
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        /*Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        primaryStage.show();*/
     }
 
 
